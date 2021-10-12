a94842a0376c81e4695c5d661499a4fc2bb1765a
Vish O <v.opatha@gmail.com>
2021-10-12 12:16:51 +1030

    Update server.js to use routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDb", {
   useNewUrlParser: true,
