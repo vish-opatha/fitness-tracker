48f7cad6b175d38502ff84d3975c1e5b9f2bd756
Vish O <v.opatha@gmail.com>
2021-10-12 13:32:29 +1030

    Remove unnecessary comments

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -2,15 +2,11 @@ const router = require("express").Router();
 const path = require("path");
 
 router.use("/exercise", (req, res) => {
-  console.log("before exercise");
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
-  console.log("after exercise");
 });
 
 router.use("/stats", (req, res) => {
-  console.log("before stats");
   res.sendFile(path.join(__dirname, "../public/stats.html"));
-  console.log("after stats");
 });
 
 module.exports = router;
