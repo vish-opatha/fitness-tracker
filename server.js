36838d8ef68580aec74c887c0badb434a2785636
Vish O <v.opatha@gmail.com>
2021-10-12 13:36:55 +1030

    Include the usage of router properly

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const logger = require("morgan");
+const router = require("./routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,16 +9,17 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDb", {
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
 
 // routes
 // app.use(require("./routes/api.js"));
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
