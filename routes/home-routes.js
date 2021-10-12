dcafeff31f6570b1b888bc36dff4dc2cf394d071
Vish O <v.opatha@gmail.com>
2021-10-12 23:21:50 +1030

    Change home router method to get

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.use("/exercise", (req, res) => {
+router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-router.use("/stats", (req, res) => {
+router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
