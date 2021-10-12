af71e5f5d08598c525af943e475adbba1e033f61
Vish O <v.opatha@gmail.com>
2021-10-12 23:19:20 +1030

    Add api route to get all workouts

---
+++ routes/api.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+// router.get("/workouts", (req, res));
+
+// router.post("/workouts", (req, res) => {
+//   //   console.log(JSON.stringify(req.body));
+//   Workout.create({})
+//     .then((newWorkout) => {
+//       res.json(newWorkout);
+//     })
+//     .catch((e) => {
+//       res.status(400).json(e);
+//     });
+// });
+
+router.get("/all", (req, res) => {
+  Workout.find({})
+    .then((dbTransaction) => {
+      res.json(dbTransaction);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
