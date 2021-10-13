1e7835e09a42dcccce2fc5786ec0c4b56449b9b8
Vish O <v.opatha@gmail.com>
2021-10-13 12:45:51 +1030

    Update routes and change db name in seeds

--- routes/api.js
+++ routes/api.js
@@ -3,24 +3,14 @@ const Workout = require("../models/workout.js");
 
 // router.get("/workouts", (req, res));
 
-// router.post("/workouts", (req, res) => {
-//   //   console.log(JSON.stringify(req.body));
-//   Workout.create({})
-//     .then((newWorkout) => {
-//       res.json(newWorkout);
-//     })
-//     .catch((e) => {
-//       res.status(400).json(e);
-//     });
-// });
-
-router.get("/all", (req, res) => {
-  Workout.find({})
-    .then((dbTransaction) => {
-      res.json(dbTransaction);
+router.post("/workouts", (req, res) => {
+  // console.log(JSON.stringify(req.body));
+  Workout.create({})
+    .then((newWorkout) => {
+      res.json(newWorkout);
     })
-    .catch((err) => {
-      res.status(400).json(err);
+    .catch((e) => {
+      res.status(400).json(e);
     });
 });
 
