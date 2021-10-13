4566c98aae7be569c1c6ed03b7fba4446b162d1c
Vish O <v.opatha@gmail.com>
2021-10-13 21:39:38 +1030

    Add get route for /api/workouts

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,23 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-// router.get("/workouts", (req, res));
-
-router.post("/workouts", (req, res) => {
-  // console.log(JSON.stringify(req.body));
-  Workout.create({})
-    .then((newWorkout) => {
-      res.json(newWorkout);
+// Get route for /api/workouts
+router.get("/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+    {
+      $sort: { day: 1 },
+    },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout.slice(dbWorkout.length - 7));
     })
-    .catch((e) => {
-      res.status(400).json(e);
+    .catch((err) => {
+      res.status(400).json(err);
     });
 });
 
